4119962545bac21eb9dd7acf6b5f2a7a1e5690c0
a21sahfa <a21sahfa@student.his.se>
2022-05-04 15:34:42 +0200

    skrev kod

--- app/src/main/java/com/example/screens/MainActivity2.java
+++ app/src/main/java/com/example/screens/MainActivity2.java
@@ -3,12 +3,17 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        text = findViewById(R.id.textView);
+        text.setText(getIntent().getStringExtra("tjena"));
     }
 }
